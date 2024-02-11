aef039e53008e705a8d922fa3d706d6acb9b6c2a
David Prskavec <david.prskavec@etnetera.eu>
2024-02-11 23:07:30 +0100

    refactor: major refactor WiP

--- src/layout/components/Header/Header.tsx
+++ src/layout/components/Header/Header.tsx
@@ -1,11 +1,12 @@
-import Link from 'next/link';
 import { FC } from 'react';
 
 import { useAuth } from '@/hooks';
 
-import { HeaderLoggedIn, HeaderLoggedOut, links } from '@/layout/components';
-
-import Logo from '~/images/logo.svg';
+import {
+  HeaderLinks,
+  HeaderLoggedIn,
+  HeaderLoggedOut,
+} from '@/layout/components';
 
 export const Header: FC = () => {
   const { user, logOut } = useAuth();
@@ -13,26 +14,8 @@ export const Header: FC = () => {
   return (
     <header className='sticky top-0 z-50 bg-[#F8F9FA]' role='banner'>
       <div className='layout flex h-14 items-center justify-between'>
-        <nav aria-label='Main navigation'>
-          <ul className='flex items-center justify-between gap-10'>
-            <Link href='/' aria-label='Go to homepage'>
-              <Logo height='2.25em' width='2.25em' aria-hidden='true' />
-            </Link>
-            {links.map((item) => (
-              <li
-                key={`${item.href}${item.label}`}
-                className='text-mediumGrey hover:text-gray-600'
-              >
-                <Link
-                  href={item.href}
-                  aria-label={`Go to ${item.label.toLowerCase()}`}
-                >
-                  {item.label}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
+        <HeaderLinks />
+
         {user ? <HeaderLoggedIn logOut={logOut} /> : <HeaderLoggedOut />}
       </div>
     </header>
